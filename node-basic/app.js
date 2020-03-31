833c19499ac4bebf5b42e69c9401c88dea280b5a
HrithikMittal <adhikanshmittal@gmail.com>
2020-03-31 22:56:17 +0530

    check exports

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,9 +1,8 @@
 // console.log("Hello from node js");
 
-function sum(a, b) {
-  return a + b;
-}
+var fun = require("../helper.js");
 
 // var let const
-const total = sum(2, 3);
+console.log(process);
+const total = fun.sum(2, 3);
 console.log(total);
