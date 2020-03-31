beec891ec4207b60751b8d5922bcb254aef36e9e
HrithikMittal <adhikanshmittal@gmail.com>
2020-03-31 23:52:12 +0530

    Sync and Async

--- fileSystem.js
+++ fileSystem.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
 const fileName = "./file.txt";
 
-fs.watch(fileName, () => {
-  console.log("There is some change in file");
-});
+// fs.watch(fileName, () => {
+//   console.log("There is some change in file");
+// });
+
+console.log("Hello This is Step 1");
+
+// fs.readFile(fileName, (err, message) => {
+//   if (err) {
+//     console.log("Error is ", err.message);
+//   } else {
+//     console.log(message.toString());
+//   }
+// });
+
+const filetext = fs.readFileSync(fileName);
+console.log(filetext.toString());
+
+console.log("Hello This is Step 2");
