449668c80f91fd2dfc63a54518d34a5aad290f4c
HrithikMittal <adhikanshmittal@gmail.com>
2020-04-01 17:23:33 +0530

    connection to the Database

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var dotenv = require("dotenv").config();
 var morgan = require("morgan");
+var mongoose = require("mongoose");
 var app = express();
 var port = process.env.PORT;
 
 var postRoutes = require("./routes/post");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Database is connected successfully");
+  })
+  .catch(err => {
+    console.log("Error is ", err.message);
+  });
 
 app.use(morgan("dev"));
 app.use("/", postRoutes);
