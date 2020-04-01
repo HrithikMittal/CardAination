1433ff0c1ca9182aaf13220a07f640e226b40a22
HrithikMittal <adhikanshmittal@gmail.com>
2020-04-01 18:33:36 +0530

    add validator

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var postControllers = require("../controllers/post");
+var postValidation = require("../helpers/index");
 
 router.get("/", postControllers.getPosts);
-router.post("/post", postControllers.createPost);
+router.post(
+  "/post",
+  postValidation.createPostValidator,
+  postControllers.createPost
+);
 
 module.exports = router;
