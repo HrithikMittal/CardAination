51573eb96f7b125814ad0ea9895ef6334229f032
HrithikMittal <adhikanshmittal@gmail.com>
2020-04-02 11:20:26 +0530

    login and Signup

--- controllers/user.js
+++ controllers/user.js
@@ -3,14 +3,49 @@ var User = require("../Models/User");
 
 const signup = (req, res) => {
   var newUser = new User(req.body);
-  newUser
-    .save()
-    .then(user => {
-      res.json({ user });
+
+  User.findOne({ email: newUser.email })
+    .then(userres => {
+      if (userres)
+        res.json({
+          message: "User is already signup with this email Id"
+        });
+      else {
+        bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            if (err) res.josn({ message: "Error in hashing the password" });
+            newUser
+              .save()
+              .then(user => {
+                res.json({ user });
+              })
+              .catch(err => {
+                res.json({ Error: err, "Error message": err.message });
+              });
+          });
+        });
+      }
     })
     .catch(err => {
-      res.josn({ Error: err, "Error message": err.message });
+      res.json({ message: "Error is in air" });
     });
 };
 
-module.exports = { signup };
+const login = (req, res) => {
+  var user = req.body;
+
+  User.findOne({ email: user.email })
+    .then(person => {
+      bcrypt.compare(user.password, person.password, function(err, result) {
+        if (!result) res.json({ message: "Wrong Password" });
+
+        res.json({ message: "You are Authenticated" });
+      });
+    })
+    .catch(err => {
+      res.json({ message: err.message });
+    });
+};
+
+module.exports = { signup, login };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var userController = require("../controllers/user");
 
-router.post("/sign", userController.signup);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
 
 module.exports = router;
